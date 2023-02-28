9737712da695f31432f5844aec929cb2dc053052
Jaiswal <jaya.jaiswal@accenture.com>
2023-02-28 19:59:10 +0100

    changes for lambok

--- src/main/java/com/accenture/carbonFootprintCalculator/model/EmberData.java
+++ src/main/java/com/accenture/carbonFootprintCalculator/model/EmberData.java
@@ -1,7 +1,11 @@
 package com.accenture.carbonFootprintCalculator.model;
 
 import com.opencsv.bean.CsvBindByName;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class EmberData {
     @CsvBindByName(column= "country_or_region")
     private String country;
@@ -14,38 +18,6 @@ public class EmberData {
 
     @CsvBindByName(column = "share_of_generation_pct")
     private Double generationPercent;
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getTypeOfEnergy() {
-        return typeOfEnergy;
-    }
-
-    public void setTypeOfEnergy(String typeOfEnergy) {
-        this.typeOfEnergy = typeOfEnergy;
-    }
-
-    public Double getGenerationPercent() {
-        return generationPercent;
-    }
-
-    public void setGenerationPercent(Double generationPercent) {
-        this.generationPercent = generationPercent;
-    }
 
     @Override
     public String toString() {
